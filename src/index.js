90d4611b06fb3810815016bbf88f4932df87f6c7
Jenny Espitia <jennyme971213@gmail.com>
2022-06-28 15:16:23 -0500

    cambios css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import { Provider } from 'react-redux';
 import AppRouter from './routes/AppRouter';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css';
+import './styles/StylesGlobals'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Buy from '../components/Buy';
+import Choose from '../components/Choose';
+import Intro from '../components/Intro';
+import Sell from '../components/Sell';
 import Home from '../containers/Home';
 
 const AppRouter = () => {
@@ -8,6 +12,11 @@ const AppRouter = () => {
       <Routes>
         <Route path='/' element={<Home />} />
 
+
+        <Route path='/intro' element={<Intro />} />
+        <Route path='/choose' element={<Choose />} />
+        <Route path='/sell' element={<Sell />} />
+        <Route path='/buy' element={<Buy />} />
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </BrowserRouter>
