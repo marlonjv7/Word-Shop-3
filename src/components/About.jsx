2ba7f3fe08299d30c1bfe2c9a45c11fa14fa3f30
Danna Bastidas <dannabas2001@gmail.com>
2022-06-28 17:37:50 -0500

    pantallas

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Contenedor } from '../styles/Terms'
+
+const About = () => {
+  return (
+    <Contenedor><h3 style={{textAlign:'center'}}>About Us</h3><img width='20%'style={{alignSelf:'center'}} src="https://res.cloudinary.com/dg29vcpk7/image/upload/v1656444935/Bulletinboard/Logo_3_hdfpgj.png" alt="" /><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida dictum fusce ut placerat orci nulla pellentesque dignissim enim. Nisl vel pretium lectus quam id leo. Convallis posuere morbi leo urna molestie at elementum eu facilisis. Non diam phasellus vestibulum lorem sed. Scelerisque purus semper eget duis at tellus at. Commodo odio aenean sed adipiscing diam donec. Pharetra et ultrices neque ornare aenean. In egestas erat imperdiet sed euismod nisi porta lorem. Pulvinar mattis nunc sed blandit libero volutpat sed. Accumsan lacus vel facilisis volutpat est velit egestas. Sem viverra aliquet eget sit amet. Cursus turpis massa tincidunt dui ut ornare lectus sit. Enim facilisis gravida neque convallis a.<br/>
+    Eu scelerisque felis imperdiet proin fermentum leo. Libero volutpat sed cras ornare arcu dui vivamus arcu felis. Laoreet id donec ultrices tincidunt. Diam sollicitudin tempor id eu. Pharetra pharetra massa massa ultricies mi quis. Sapien eget mi proin sed libero enim sed faucibus. Dolor magna eget est lorem. Egestas diam in arcu cursus euismod quis viverra nibh. Felis eget nunc lobortis mattis aliquam faucibus purus in. Aliquet enim tortor at auctor urna nunc. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Augue ut lectus arcu bibendum at varius vel pharetra. Faucibus scelerisque eleifend donec pretium. Hac habitasse platea dictumst quisque. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Integer vitae justo eget magna. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Nisl purus in mollis nunc sed. Volutpat sed cras ornare arcu dui vivamus.
+    
+    
+    </p></Contenedor>
+  )
+}
+
+export default About
