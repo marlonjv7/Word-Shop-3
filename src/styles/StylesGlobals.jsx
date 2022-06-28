51797d7f2f393455ae87f4f014a1376438b01622
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-06-28 15:08:17 -0500

    neuevos cambios

--- src/styles/StylesGlobals.jsx
+++ src/styles/StylesGlobals.jsx
@@ -3,3 +3,12 @@ import styled from "styled-components";
 export const H1 = styled.h1`
 color: blue;
 `
+
+export const NavBarSup = styled.div`
+max-width: 350px;
+margin: 0 auto;
+    @media (min-width: 700px){
+        margin: 0 10vw;
+        max-width: 878px;
+    }
+`
