53fa876f69d6e6a8769c5bd83ade34d8de8fc188
Jenny Espitia <jennyme971213@gmail.com>
2022-06-28 16:04:52 -0500

    cambios

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 // import NavBar from '../components/NavBar';
-import NavBarSup from '../components/NavBarSup';
 import { H1 } from '../styles/StylesGlobals';
 
 const Home = () => {
     return (
         <div>
-            <NavBarSup />
             <H1 className='text-center'>Home</H1>
         </div>
     )
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,7 +5,7 @@ import Choose from '../components/Choose';
 import Intro from '../components/Intro';
 import Sell from '../components/Sell';
 import Home from '../containers/Home';
-import AgregarEjercisio from '../components/AgregarEjercisio';
+// import AgregarEjercisio from '../components/AgregarEjercisio';
 
 
 
@@ -20,7 +20,7 @@ const AppRouter = () => {
         <Route path='/choose' element={<Choose />} />
         <Route path='/sell' element={<Sell />} />
         <Route path='/buy' element={<Buy />} />
-        <Route path="/AgregarEjercisio" element={<AgregarEjercisio/>}/>
+        {/* <Route path="/AgregarEjercisio" element={<AgregarEjercisio/>}/> */}
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </BrowserRouter>
