adb23b665897d43e44c83d63e74af79f10128bc2
Jenny Espitia <jennyme971213@gmail.com>
2022-06-28 16:33:25 -0500

    cambios

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,15 @@
+import { Button } from '@mui/material';
 import React from 'react'
 // import NavBar from '../components/NavBar';
 import { H1 } from '../styles/StylesGlobals';
 
 const Home = () => {
+    // const dispatch = useDispatch();
     return (
         <div>
           
             <H1 className='text-center'>Home</H1>
+            {/* <Button onClick={() => dispatch(logoutActionAsync())}>Logout</Button> */}
         </div>
     )
 }
