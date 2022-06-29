aed61a1e91cfb56563ef7ea60c51714513803303
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-06-29 14:15:15 -0500

    nuevo commit

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,11 +12,6 @@ const Home = () => {
         <div>
             <H1 className='text-center'>Home</H1>
             <Button onClick={() => dispatch(logoutActionAsync())}>Logout</Button>
-            <h3>feacture</h3>
-            <h2>hola</h2>
-            <h4>release</h4>
-            <h5>develop</h5>
-
             <Main />
         </div>
     )
